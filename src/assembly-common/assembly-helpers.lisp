9af7ceebb2983114c8f79dbbae9269cfb822ddb1
Ties Stuij <ties@stuij.se>
2008-06-12 10:03:04 +0000

    changed jr referencing implementation to make use of armish address resolving. Which makes everything MUCH less messy
    darcs-hash:20080612100304-f86be-0c3f9888bc936429a2f9d5faeff482233ac4a284

--- src/assembly-common/assembly-helpers.lisp
+++ src/assembly-common/assembly-helpers.lisp
@@ -1,42 +1,50 @@
 (in-package :liards)
 
-;; general lookup-table functionality reachable from register
-(defvar *jr*) ;; like in Dallas, the reg with the contacts
+(def-space-n-clusters-n-blocks liards
+  ()
+  (liards-common liards-writer))
 
-(defvar *jr-hash*)
-(defvar *jr-address*)
+;; general lookup-table functionality reachable from register
+(defparameter *jr* 'r0) ;; like in Dallas, the reg with the contacts
 
 (defparameter *jr-reachables*
-  '(text-color
-    text-bg-color
-    text-line-nr
-    text-line-pos
+  '((text-color (word #xFFFF))
+    (text-bg-color (word #x0))
+    (text-line-nr (word 0))
+    (text-line-pos (word 0))
 
-    char-x-data
-    char-y-data
-    char-sizes
-    char-offsets
-    char-widths))
+    (char-x-data (word (address :char-x-data)))
+    (char-y-data (word (address :char-y-data)))
+    (char-sizes (word (address :char-sizes)))
+    (char-offsets (word (address :char-offsets)))
+    (char-widths (word (address :char-widths)))))
 
 (defun add-jr-reachables (new-reachables)
-  (setf *jr-reachables*
-        (append *jr-reachables* (convert-to-intern new-reachables :liards))))
+  (let ((clean-new-reachables (convert-to-intern (ensure-list new-reachables) :liards)))
+    (if (intersection *jr-reachables* clean-new-reachables :test (lambda (r1 r2)
+                                                                   (eql (car r1) (car r2))))
+        
+        (error "jr reachables ~a and new reachables ~a share identifiers"
+               *jr-reachables* clean-new-reachables)
+        (setf *jr-reachables* (append *jr-reachables* clean-new-reachables)))))
 
 (defun get-jr-offset (thing)
   "reference offset address reachable from *jr*"
   (let ((internal-thing (intern (format nil "~a" thing) :liards)))
-    (aif (gethash internal-thing *jr-hash*)
+    (aif (position internal-thing *jr-reachables* :test (lambda (r1 r2)
+                                                          (eql (car r2) r1)))
          it
-         (error "jr offset request has hit upon uninitialized value ~A. please correct. thank you."
+         (error "jr offset request has hit upon unknown offset value ~A. please correct. thank you."
                 internal-thing))))
 
-(defun set-jr (thing val)
-  (let ((internal-thing (intern (format nil "~a" thing) :liards)))
-    (if (position internal-thing *jr-reachables*)
-        (setf (gethash internal-thing *jr-hash*) val)
-        (error "~A is not reachable through jr" internal-thing))))
+(def-asm-macro load-jr (dest-reg identifier)
+  `((ldr ,dest-reg (,*jr* ,(* 4 (get-jr-offset identifier))))))
 
-(defun check-jr-reachables ()
-  (loop for reachable in *jr-reachables*
-     do (if (not (gethash reachable *jr-hash*))
-            (error "jr-reachable ~A hasn't been set" reachable))))
+(def-asm-macro store-jr (val-reg identifier)
+  `((str ,val-reg (,*jr* ,(* 4 (get-jr-offset identifier))))))
+
+(def-asm-macro dump-jr-data ()
+  `(:jr-base
+    ,@(loop for item in *jr-reachables*
+         collect `,(cadr item))))
+
